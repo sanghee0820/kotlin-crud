2a5e85ff243935626e2a48d9d1b420d5129ad61e
sanghee0820 <sanghee0820.dev@gmail.com>
2025-06-21 18:55:39 +0900

    init

---
+++ src/test/kotlin/practice/kotlincrud/KotlinCrudApplicationTests.kt
@@ -0,0 +1,13 @@
+package practice.kotlincrud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinCrudApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
